b20280c7fcb737d00f3edd4e82845a2598b09d34
Matt Ryan <matt@mvryan.org>
2019-11-26 04:55:10 -0700

    Add command-line help support.

--- src/main/java/com/zoomulus/cli/CLI.java
+++ src/main/java/com/zoomulus/cli/CLI.java
@@ -17,20 +17,24 @@
 
 package com.zoomulus.cli;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.google.common.reflect.ClassPath;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * The Zoomulus command-line interface driver.
@@ -42,6 +46,7 @@ public class CLI {
     private static Logger LOG = LoggerFactory.getLogger(CLI.class);
 
     private String prompt;
+    private Set<String> helpStrings = Sets.newHashSet("?", "help");
     private Map<String, Command> commands = Maps.newHashMap();
 
     /**
@@ -65,7 +70,7 @@ public class CLI {
 
     private void addCommand(@NotNull final Command command) {
         for (String commandName : command.getNames()) {
-            commands.put(commandName, command);
+            commands.put(commandName.toLowerCase(), command);
         }
     }
 
@@ -140,26 +145,49 @@ public class CLI {
      * Run the CLI.
      */
     public void run() {
+        @NotNull final PrintStream out = System.out;
+        @NotNull final PrintStream err = System.err;
         while (true) {
-            System.out.print(prompt);
+            out.print(prompt);
             Scanner scanner = new Scanner(System.in);
             String nextLine = scanner.nextLine();
             if (! Strings.isNullOrEmpty(nextLine)) {
-                String[] input = nextLine.split("\\s+", 1);
-                String commandName = input[0];
+                String[] input = nextLine.split("\\s+", 2);
+                String commandName = input[0].toLowerCase();
                 List<String> args = Lists.newArrayList();
                 if (input.length > 1) {
                     args.addAll(Arrays.asList(input[1].split("\\s+")));
                 }
+                if (helpStrings.contains(commandName)) {
+                    printHelp(out);
+                    continue;
+                }
+
                 Command command = commands.get(commandName);
                 if (null == command) {
                     LOG.warn("No such command '{}' registered", commandName);
-                } else {
-                    if (!command.run(commandName, args)) {
+                }
+                else {
+                    if (args.size() > 0 && helpStrings.contains(args.get(0).toLowerCase().trim())) {
+                        out.println(String.format("%s command help:\n", commandName));
+                        out.println("Command forms: " + Joiner.on(", ").join(command.getNames()));
+                        if (command.getLongDescription().isPresent()) {
+                            out.println(command.getLongDescription().get());
+                        }
+                    }
+                    else if (!command.run(commandName, args, out, err)) {
                         break;
                     }
                 }
             }
         }
+    }
+
+    private void printHelp(@NotNull final PrintStream out) {
+        OutputTable outputTable = new OutputTable(out).withLines(false);
+        for (Command command : Sets.newHashSet(commands.values())) {
+            outputTable.addRow(Joiner.on(", ").join(command.getNames()), command.getShortDescription());
+        }
+        outputTable.print();
     }
 }
--- src/main/java/com/zoomulus/cli/Command.java
+++ src/main/java/com/zoomulus/cli/Command.java
@@ -19,7 +19,9 @@ package com.zoomulus.cli;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.PrintStream;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Commands for the Zoomulus {@link CLI}.
@@ -36,11 +38,31 @@ public interface Command {
     @NotNull List<String> getNames();
 
     /**
+     * Returns short descriptive text that briefly describes the command.  Used when the "help" command is called.  This
+     * should ideally be just one line, just a few words of text.
+     *
+     * @return description text.
+     */
+    @NotNull String getShortDescription();
+
+    /**
+     * Returns text describing the command.  Used when calling "[command-name] help".
+     *
+     * @return description text.
+     */
+    @NotNull Optional<String> getLongDescription();
+
+    /**
      * Executes the command.
      *
      * @param commandName The command name that was used to select the command.
      * @param args The command-line arguments provided to the command.
+     * @param out A {@link PrintStream} for writing standard output.
+     * @param err A {@link PrintStream} for writing error output.
      * @return false if the CLI should exit; true otherwise.  Most commands will return true.
      */
-    boolean run(@NotNull final String commandName, @NotNull final List<String> args);
+    boolean run(@NotNull final String commandName,
+                @NotNull final List<String> args,
+                @NotNull final PrintStream out,
+                @NotNull final PrintStream err);
 }
